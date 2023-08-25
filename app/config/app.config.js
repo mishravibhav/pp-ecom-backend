c35fc6f3a47a44eda01c2049411f56b0fee176ab
Vibhav Mishra <vibhavmishra@Vibhav-Mishra.local>
2023-08-26 01:38:57 +0530

    mongodb authentication addedd

--- app/config/app.config.js
+++ app/config/app.config.js
@@ -5,15 +5,15 @@ require('dotenv').config({ path: path.resolve(__dirname, `../../${env}.env`) })
 module.exports = {
     database :{
         mysql:{
-            host : process.env.host, 
-            user:process.env.user,
-            password:process.env.password,
-            database:process.env.database,
-            port :process.env.port,
-            connectionLimit : process.env.connectionlimit
+            host : process.env.sql_host, 
+            user:process.env.sql_user,
+            password:process.env.sql_password,
+            database:process.env.sql_database,
+            port :process.env.sql_port,
+            connectionLimit : process.env.sql_connectionlimit
         },
         mongodb:{
-            url:"mongodb://localhost:27017/ecom"
+            url:`mongodb://${process.env.mongodb_user}:${process.env.mongodb_password}@${process.env.mongodb_host}:${process.env.mongodb_port}/${process.env.mongodb_database}`
         },
     },
     secretkey:"secretkey"
