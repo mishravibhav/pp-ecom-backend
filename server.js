aa1289df95b30fa9f6b160546f3fb23b2d54b139
Vibhav Mishra <vibhavmishra@Vibhav-Mishra.local>
2023-08-25 16:01:07 +0530

    response timer addedd

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path')
 var cookieParser = require('cookie-parser');
+const responseTime = require('response-time')
 const app = express()
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '/app')))
 app.use(cookieParser());
+
+app.use(responseTime((req, res, time) => {
+  console.log(req.method, req.url, time + 'ms');
+}));
 
 const port = 3000
 
@@ -14,14 +19,10 @@ require('./app/database/mongo/connection.mongo.database')
 // routes
 app.use('/',require('./app/routes/index.route'))
 
-// app.use(errorLogger)
-// app.use(errorResponder)
-// app.use(invalidPathHandler)
 
 app.all('*', (req, res, next) => {
 
     res.status(404).send({ success: false, error:`Can't find ${req.originalUrl} on the server`});
-    // next();
   });
 
 app.listen(port,()=>{
