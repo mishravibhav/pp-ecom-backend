2354e2cd2d5ea1bee04662eb3314289dc956dca2
Vibhav Mishra <mishravibhav1910@gmail.com>
2023-08-29 23:54:53 +0530

    .

--- app/utils/mongo.util.js
+++ app/utils/mongo.util.js
@@ -9,3 +9,29 @@
 //         }
 //     }
 // ]).pretty();
+
+// db.collection.aggrigate([{$match:{_id:"id"}},{
+//     $lookup:{
+//         from:"events",
+//         localField:"_id",
+//         foreignField:"user_id",
+//         as :"attributes"
+//     }
+// }])
+
+
+// pipeline: [
+//     { $match: {
+//         $expr: { $and: [
+//             { $in: [ 8, "$all_category_id" ] },
+//             { $eq: [ "$article_id", "$$article_id" ] }
+//         ] }
+//     } }
+//   ],
+//   as: "article_category"
+// } },
+// { $match: {
+//   $expr: { $gt: [
+//     { $size: "$article_category"},
+//     0
+//   ] }
